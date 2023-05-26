9203667c3c6b629ba93df0b2fb80014feb787bd6
csiejimmyliu <r10922129@csie.ntu.edu.tw>
2023-05-26 10:22:16 +0800

    finetune init

---
+++ finetune_SD/model/encoder_decoder.py
@@ -0,0 +1,51 @@
+import torch.nn as nn
+from model.encoder import Encoder
+from model.decoder import Decoder
+from options import HiDDenConfiguration
+from noise_layers.noiser import Noiser
+import torch
+
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+import torch
+
+from noise_layers.jpeg_compression import rgb2yuv_tensor
+
+model_id = "runwayml/stable-diffusion-v1-5"
+
+class EncoderDecoder(nn.Module):
+    """
+    Combines Encoder->Noiser->Decoder into single pipeline.
+    The input is the cover image and the watermark message. The module inserts the watermark into the image
+    (obtaining encoded_image), then applies Noise layers (obtaining noised_image), then passes the noised_image
+    to the Decoder which tries to recover the watermark (called decoded_message). The module outputs
+    a three-tuple: (encoded_image, noised_image, decoded_message)
+    """
+    def __init__(self, config: HiDDenConfiguration, noiser: Noiser):
+
+        super(EncoderDecoder, self).__init__()
+        pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+        self.encoder = pipe.vae
+        self.noiser = noiser
+        self.alpha=config.alpha
+        self.decoder = Decoder(config)
+
+    def forward(self, image, message):
+        #encoded_image = self.encoder(image, message)
+        watermark_noise=self.encoder(image).sample
+        #watermark_noise=torch.tanh(watermark_noise)
+        #encoded_image=image+self.alpha*watermark_noise
+        encoded_image=watermark_noise
+
+
+        #to [-1,1] yuv
+        encoded_image=encoded_image*2.0-1.0
+        encoded_image=rgb2yuv_tensor(encoded_image)
+
+        yuv_image=image*2.0-1.0
+        yuv_image=rgb2yuv_tensor(yuv_image)
+        
+        noised_and_cover = self.noiser([encoded_image, yuv_image])
+        noised_image = noised_and_cover[0]
+        decoded_message = self.decoder(noised_image)
+        return encoded_image, noised_image, decoded_message
---
+++ finetune_SD/utils.py
@@ -0,0 +1,266 @@
+import numpy as np
+import os
+import re
+import csv
+import time
+import pickle
+import logging
+
+import torch
+from torchvision import datasets, transforms
+import torchvision.utils
+from torch.utils import data
+import torch.nn.functional as F
+
+from options import HiDDenConfiguration, TrainingOptions
+from model.hidden import Hidden
+from skimage import color
+
+from typing import Any, BinaryIO, List, Optional, Tuple, Union
+import pathlib
+from torchvision.utils import _log_api_usage_once,make_grid
+from PIL import Image
+
+
+def ycbcr2rgb(im):
+    xform = np.array([[1, 0, 1.402], [1, -0.34414, -.71414], [1, 1.772, 0]])
+    rgb = im
+    rgb[:,:,[1,2]] -= 128
+    rgb = rgb.dot(xform.T)
+    np.putmask(rgb, rgb > 255, 255)
+    np.putmask(rgb, rgb < 0, 0)
+    return np.uint8(rgb)
+
+
+
+def save_ycbcr_img(    tensor: Union[torch.Tensor, List[torch.Tensor]],
+    fp: Union[str, pathlib.Path, BinaryIO],
+    format: Optional[str] = None,
+    **kwargs,
+) -> None:
+    """
+    Save a given Tensor into an image file.
+
+    Args:
+        tensor (Tensor or list): Image to be saved. If given a mini-batch tensor,
+            saves the tensor as a grid of images by calling ``make_grid``.
+        fp (string or file object): A filename or a file object
+        format(Optional):  If omitted, the format to use is determined from the filename extension.
+            If a file object was used instead of a filename, this parameter should always be used.
+        **kwargs: Other arguments are documented in ``make_grid``.
+    """
+
+    if not torch.jit.is_scripting() and not torch.jit.is_tracing():
+        _log_api_usage_once(save_ycbcr_img)
+    grid = make_grid(tensor, **kwargs)
+    # Add 0.5 after unnormalizing to [0, 255] to round to nearest integer
+    ndarr = grid.mul(255).add_(0.5).permute(1, 2, 0).to("cpu").numpy()
+    ndarr=ycbcr2rgb(ndarr)
+    im = Image.fromarray(ndarr)
+    im.save(fp, format=format)
+
+class ImageFolderInstance(datasets.ImageFolder):
+    """Folder datasets which returns the index of the image as well
+    """
+
+    def __init__(self, root, transform=None, target_transform=None):
+        super(ImageFolderInstance, self).__init__(root, transform, target_transform)
+
+
+
+    
+    def __getitem__(self, index: int):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        
+        sample = sample.convert("YCbCr")
+
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target
+
+def image_to_tensor(image):
+    """
+    Transforms a numpy-image into torch tensor
+    :param image: (batch_size x height x width x channels) uint8 array
+    :return: (batch_size x channels x height x width) torch tensor in range [-1.0, 1.0]
+    """
+    image_tensor = torch.Tensor(image)
+    image_tensor.unsqueeze_(0)
+    image_tensor = image_tensor.permute(0, 3, 1, 2)
+    image_tensor = image_tensor / 127.5 - 1
+    return image_tensor
+
+
+def tensor_to_image(tensor):
+    """
+    Transforms a torch tensor into numpy uint8 array (image)
+    :param tensor: (batch_size x channels x height x width) torch tensor in range [-1.0, 1.0]
+    :return: (batch_size x height x width x channels) uint8 array
+    """
+    image = tensor.permute(0, 2, 3, 1).cpu().numpy()
+    image = (image + 1) * 127.5
+    return np.clip(image, 0, 255).astype(np.uint8)
+
+
+def save_images(original_images, watermarked_images, epoch, folder, resize_to=None):
+    images = original_images[:original_images.shape[0], :, :, :].cpu()
+    watermarked_images = watermarked_images[:watermarked_images.shape[0], :, :, :].cpu()
+
+    # scale values to range [0, 1] from original range of [-1, 1]
+    images = (images + 1) / 2
+    watermarked_images = (watermarked_images + 1) / 2
+
+    if resize_to is not None:
+        images = F.interpolate(images, size=resize_to)
+        watermarked_images = F.interpolate(watermarked_images, size=resize_to)
+
+    stacked_images = torch.cat([images, watermarked_images], dim=0)
+    filename = os.path.join(folder, 'epoch-{}.png'.format(epoch))
+    #torchvision.utils.save_image(stacked_images, filename)
+    save_ycbcr_img(stacked_images, filename)
+    
+
+def sorted_nicely(l):
+    """ Sort the given iterable in the way that humans expect."""
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
+
+
+def last_checkpoint_from_folder(folder: str):
+    last_file = sorted_nicely(os.listdir(folder))[-1]
+    last_file = os.path.join(folder, last_file)
+    return last_file
+
+
+def save_checkpoint(model: Hidden, experiment_name: str, epoch: int, checkpoint_folder: str):
+    """ Saves a checkpoint at the end of an epoch. """
+    if not os.path.exists(checkpoint_folder):
+        os.makedirs(checkpoint_folder)
+
+    checkpoint_filename = f'{experiment_name}--epoch-{epoch}.pyt'
+    checkpoint_filename = os.path.join(checkpoint_folder, checkpoint_filename)
+    logging.info('Saving checkpoint to {}'.format(checkpoint_filename))
+    checkpoint = {
+        'enc-dec-model': model.encoder_decoder.state_dict(),
+        'enc-dec-optim': model.optimizer_enc_dec.state_dict(),
+        #'discrim-model': model.discriminator.state_dict(),
+        #'discrim-optim': model.optimizer_discrim.state_dict(),
+        'epoch': epoch
+    }
+    torch.save(checkpoint, checkpoint_filename)
+    logging.info('Saving checkpoint done.')
+
+
+# def load_checkpoint(hidden_net: Hidden, options: Options, this_run_folder: str):
+def load_last_checkpoint(checkpoint_folder):
+    """ Load the last checkpoint from the given folder """
+    last_checkpoint_file = last_checkpoint_from_folder(checkpoint_folder)
+    checkpoint = torch.load(last_checkpoint_file)
+
+    return checkpoint, last_checkpoint_file
+
+
+def load_checkpoint(checkpoint_path):
+    """ Load the last checkpoint from the given folder """
+    last_checkpoint_file = checkpoint_path
+    checkpoint = torch.load(last_checkpoint_file)
+
+    return checkpoint, last_checkpoint_file
+
+
+def model_from_checkpoint(hidden_net, checkpoint):
+    """ Restores the hidden_net object from a checkpoint object """
+    hidden_net.encoder_decoder.load_state_dict(checkpoint['enc-dec-model'])
+    hidden_net.optimizer_enc_dec.load_state_dict(checkpoint['enc-dec-optim'])
+    #hidden_net.discriminator.load_state_dict(checkpoint['discrim-model'])
+    #hidden_net.optimizer_discrim.load_state_dict(checkpoint['discrim-optim'])
+
+
+def load_options(options_file_name) -> (TrainingOptions, HiDDenConfiguration, dict):
+    """ Loads the training, model, and noise configurations from the given folder """
+    with open(os.path.join(options_file_name), 'rb') as f:
+        train_options = pickle.load(f)
+        noise_config = pickle.load(f)
+        hidden_config = pickle.load(f)
+        # for backward-capability. Some models were trained and saved before .enable_fp16 was added
+        if not hasattr(hidden_config, 'enable_fp16'):
+            setattr(hidden_config, 'enable_fp16', False)
+
+    return train_options, hidden_config, noise_config
+
+
+def get_data_loaders(hidden_config: HiDDenConfiguration, train_options: TrainingOptions):
+    """ Get torch data loaders for training and validation. The data loaders take a crop of the image,
+    transform it into tensor, and normalize it."""
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomCrop((hidden_config.H, hidden_config.W), pad_if_needed=True),
+            transforms.ToTensor(),
+            #transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ]),
+        'test': transforms.Compose([
+            transforms.CenterCrop((hidden_config.H, hidden_config.W)),
+            transforms.ToTensor(),
+            #transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ])
+    }
+
+    train_images = datasets.ImageFolder(train_options.train_folder, data_transforms['train'])
+    train_loader = torch.utils.data.DataLoader(train_images, batch_size=train_options.batch_size, shuffle=True,
+                                               num_workers=4)
+
+    validation_images = datasets.ImageFolder(train_options.validation_folder, data_transforms['test'])
+    validation_loader = torch.utils.data.DataLoader(validation_images, batch_size=train_options.batch_size,
+                                                    shuffle=False, num_workers=4)
+
+    return train_loader, validation_loader
+
+
+def log_progress(losses_accu):
+    log_print_helper(losses_accu, logging.info)
+
+
+def print_progress(losses_accu):
+    log_print_helper(losses_accu, print)
+
+
+def log_print_helper(losses_accu, log_or_print_func):
+    max_len = max([len(loss_name) for loss_name in losses_accu])
+    for loss_name, loss_value in losses_accu.items():
+        log_or_print_func(loss_name.ljust(max_len + 4) + '{:.4f}'.format(loss_value.avg))
+
+
+def create_folder_for_run(runs_folder, experiment_name):
+    if not os.path.exists(runs_folder):
+        os.makedirs(runs_folder)
+
+    this_run_folder = os.path.join(runs_folder, f'{experiment_name} {time.strftime("%Y.%m.%d--%H-%M-%S")}')
+
+    os.makedirs(this_run_folder)
+    os.makedirs(os.path.join(this_run_folder, 'checkpoints'))
+    os.makedirs(os.path.join(this_run_folder, 'images'))
+
+    return this_run_folder
+
+
+def write_losses(file_name, losses_accu, epoch, duration):
+    with open(file_name, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        if epoch == 1:
+            row_to_write = ['epoch'] + [loss_name.strip() for loss_name in losses_accu.keys()] + ['duration']
+            writer.writerow(row_to_write)
+        row_to_write = [epoch] + ['{:.4f}'.format(loss_avg.avg) for loss_avg in losses_accu.values()] + [
+            '{:.0f}'.format(duration)]
+        writer.writerow(row_to_write)
