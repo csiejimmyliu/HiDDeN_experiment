e8c6833a89dcab30800dd6ab69a376165c1da063
csiejimmyliu <r10922129@csie.ntu.edu.tw>
2023-05-29 13:23:02 +0800

    crerate folder of gen

--- finetune_SD/fid_gen_5000.py
+++ finetune_SD/fid_gen_5000.py
@@ -22,7 +22,7 @@ import json
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 parser = argparse.ArgumentParser(description='Generate 5000 imgaes by diffusino model for fid calculation.')
-parser.add_argument('--vae_path', '-v',required=True , type=str,help='Paht of watermarked vae.')
+parser.add_argument('--vae_path', '-v', type=str,help='Paht of watermarked vae.')
 parser.add_argument('--save_folder', '-sf', required=True, type=str,help='The directory to save images.')
 parser.add_argument('--cap_path', '-c',default='../cap_list.json' , type=str,help='Paht of 5000 captions.')
 parser.add_argument('--seeds_path', '-seed',default='../5000_seeds.json' , type=str,help='Paht of 5000 seeds.')
@@ -40,9 +40,12 @@ seeds=json.load(open(args.seeds_path))
 
 pipe = StableDiffusionPipeline.from_pretrained(args.model_id, torch_dtype=torch.float32)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-tmpe_vae=AutoencoderKL.from_pretrained(args.vae_path)
-pipe.vae.decoder=tmpe_vae.decoder
-pipe.vae.post_quant_conv=tmpe_vae.post_quant_conv
+if args.vae_path==None:
+    pipe.vae=AutoencoderKL.from_pretrained("runwayml/stable-diffusion-v1-5",subfolder='vae')
+else:
+    tmpe_vae=AutoencoderKL.from_pretrained(args.vae_path)
+    pipe.vae.decoder=tmpe_vae.decoder
+    pipe.vae.post_quant_conv=tmpe_vae.post_quant_conv
 
 def dummy(images, **kwargs):
     return images, False
@@ -52,6 +55,6 @@ generator = torch.Generator(device=device)
 
 
 for i in range(int(len(seeds)/args.batch_size)):
-    output=pipe(prompt=caps[i:i+args.batch_size],height=args.size,width=args.size,guidance_scale=3.0,generator = [generator.manual_seed(gen_seed) for gen_seed in seeds[i:i+args.batch_size]])
+    output=pipe(prompt=caps[i*args.batch_size:i*args.batch_size+args.batch_size],height=args.size,width=args.size,guidance_scale=3.0,generator = [generator.manual_seed(gen_seed) for gen_seed in seeds[i*args.batch_size:i*args.batch_size+args.batch_size]])
     for j in range(args.batch_size):
         output.images[j].save(os.path.join(args.save_folder,f'{i*args.batch_size+j}.png'))
